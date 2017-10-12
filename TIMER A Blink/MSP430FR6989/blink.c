58cb330b3c5268195af792922aa3bba0c20530a8
Mike Giuliano <mikegiuliano13@gmail.com>
2017-10-12 00:04:33 -0400

    More changes
    added light up shoes

--- TIMER A Blink/MSP430FR6989/blink.c
+++ TIMER A Blink/MSP430FR6989/blink.c
@@ -1,35 +1,59 @@
 // Loads configurations for all MSP430 boards
 #include <msp430.h>
- 
-int main(void) {
-    
-  // Stops the watchdog timer
-  WDTCTL = WDTPW + WDTHOLD;
-  
-  // Disables default high-impedance mode
-  // Done by clearing the register PM5CTL0
-  // and unlocking I/O pins  (~LOCKLPM5)
-  PM5CTL0 &= ~LOCKLPM5;
-  
-  // Sets up bit 0 of P1 as output
-  P1DIR |= BIT0;
-  
-  // Initializes BIT0 to 0
-  P1OUT = 0x00;
-  
-  // Declares loop variable as volatile
-  // This prevents compiler from removing it for being "stuck" in a loop
-  // when in fact it is working as intended
-  volatile unsigned int i;
 
-  // No parameters means it loops forever
-  for (;;) {
-      
-    // Toggle bit 0 of P1 using XOR
-    P1OUT ^= BIT0;
+volatile unsigned int count = 0;
+
+void frequencyCalc(int t);
+
+void main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
+	
+	PM5CTL0 &= ~LOCKLPM5;
+
+    P1DIR = BIT0; // Set P1.0 as output
+	P9DIR = BIT7; // Set P9.7 as output
+	P1OUT = 0x00;
+	P9OUT = 0x00;
     
-    // Delay for a while
-    for (i = 0; i < 0x6000; i++); //note to self: can also use __delay_cycles(10000);
-  }
-  
+    //TA0CTL = Timer A0 chosen for use
+    //TASSEL_2 Selects SMCLK as clock source
+	//ID_2 is a clock pre-divider of 4
+    //MC_1 Count-up mode
+    TA0CCTL0 = CCIE;
+    TA0CTL = TASSEL_2 + MC_1 + ID_2;
+	
+	frequencyCalc(1);    // sets timer frequency at 1 Hz
+
+    __enable_interrupt(); // MUST BE ENABLED IN ADDITION TO GIE
+    __bis_SR_register(LPM0 + GIE); // enable interrupts in LPM0
+
+}
+
+// Function that converts 10 MHz SMCLK to desired frequency
+// Simply input the number of Hz you want as input parameter
+void frequencyCalc(int t)
+{
+    int x;
+    x = 250000 / t;
+    TA0CCR0 = x; // ex. t = 10 --> (10^6 [Hz] / 4) / 25000 = 10 Hz
+}
+
+// Interrupt subroutine
+// Called at every timer interval
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void Timer_A0(void)
+{
+
+    // Set LED blink duration according to frequencyCalc
+            P1OUT ^= BIT0; //Toggle LED
+			
+	if (count == 0)
+        {
+            P9OUT ^= BIT7; //Toggle green LED
+            count++;  // Reset count
+        }
+    else
+        count = 0;
+			
 }
